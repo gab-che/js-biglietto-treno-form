0b45f4922724e459ca7c2a35a455db9c8fcff780
gab-che <gabriellachessa1@gmail.com>
2022-10-28 18:14:10 +0200

    on button click alert shows final price

--- js/script.js
+++ js/script.js
@@ -1,31 +1,35 @@
 // ---------- informazioni che già conosco
 const price = 0.21;
 
-// ------------ logica
+// bottone
+const createTicket = document.getElementById("create_ticket");
 
-// nodo html per il nome
-const userName = document.querySelector("[name='full_name']");
+createTicket.addEventListener("click", function(){
+    
+    // nodo html per il nome
+    const userName = document.querySelector("[name='full_name']");
 
-// nodi html km ed età
-const userKm = document.querySelector("[name='km']");
+    // nodi html km ed età
+    const userKm = document.querySelector("[name='km']");
 
-const userAge = document.getElementById("age");
+    const userAge = document.getElementById("age");
 
-// calolo il prezzo base del biglietto
-const priceFull = userKm.value * price;
+    // calolo il prezzo base del biglietto
+    const priceFull = userKm.value * price;
 
-// let priceFinal = priceFull;
-// let disCount;
+    let priceFinal = priceFull;
+    let disCount;
 
-// calcolo lo sconto in base all'età
-// if (userAge < 18){
-//     disCount = .8;
-// } else if (userAge >= 65) {
-//     disCount = .6;
-// }
+    // calcolo lo sconto in base all'età
+    if (userAge.value === "young"){
+        disCount = .8;
+    } else if (userAge === "old") {
+        disCount = .6;
+    }
 
-// if (disCount){
-//     priceFinal = parseInt((priceFull * disCount).toFixed(2));
-// }
+    if (disCount){
+        priceFinal = parseInt((priceFull * disCount).toFixed(2));
+    }
 
-console.log(priceFinal)
+    alert(priceFinal);
+})

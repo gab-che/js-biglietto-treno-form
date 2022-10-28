a10cab25a50516c93088d8e56db1fb2e6e0399da
gab-che <gabriellachessa1@gmail.com>
2022-10-28 15:48:30 +0200

    added js logic

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// ---------- informazioni che già conosco
+const price = 0.21;
+
+// ------------ logica
+
+// chiedo i km ed età + li valido + li salvo in una variabile
+let userKm = parseInt(prompt("Quanti km devi fare?"));
+
+while (isNaN(userKm)){
+    userKm = parseInt(prompt("Inserisci un numero valido"));
+}
+
+let userAge = parseInt(prompt("Quanti anni hai?"));
+
+while (isNaN(userAge)){
+    userAge = parseInt(prompt("Inserisci un numero valido"));
+}
+
+// calolo il prezzo base del biglietto
+const priceFull = userKm * price;
+
+let priceFinal = priceFull;
+let disCount;
+
+// calcolo lo sconto in base all'età
+if (userAge < 18){
+    disCount = .8;
+} else if (userAge >= 65) {
+    disCount = .6;
+}
+
+if (disCount){
+    priceFinal = parseInt((priceFull * disCount).toFixed(2));
+}
+
+console.log(priceFinal)
+
+
+// ----------- nodi html

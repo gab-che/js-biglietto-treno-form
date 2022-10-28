86845949a4b66579715fff207dacc2280931bbd8
gab-che <gabriellachessa1@gmail.com>
2022-10-28 22:31:39 +0200

    fixed major bug

--- js/script.js
+++ js/script.js
@@ -1,29 +1,25 @@
-// ---------- informazioni che già conosco
 const price = 0.21;
 
-// bottone
 const createTicket = document.getElementById("create_ticket");
 
 createTicket.addEventListener("click", function(){
     
-    // nodo html per il nome
     const userName = document.querySelector("[name='full_name']");
 
-    // nodi html km ed età
     const userKm = document.querySelector("[name='km']");
 
     const userAge = document.getElementById("age");
 
-    // calolo il prezzo base del biglietto
-    const priceFull = userKm.value * price;
+    // prezzo base del biglietto
+    let priceFull = userKm.value * price;
 
     let priceFinal = priceFull;
     let disCount;
 
-    // calcolo lo sconto in base all'età
+    // sconto in base all'età
     if (userAge.value === "young"){
         disCount = .8;
-    } else if (userAge === "old") {
+    } else if (userAge.value === "old") {
         disCount = .6;
     }
 

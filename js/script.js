8b298a635aceac72923c54204e6bcf350e44b783
gab-che <gabriellachessa1@gmail.com>
2022-10-28 17:32:53 +0200

    saved html nodes

--- js/script.js
+++ js/script.js
@@ -5,38 +5,27 @@ const price = 0.21;
 
 // nodo html per il nome
 const userName = document.querySelector("[name='full_name']");
-userName.addEventListener("click", function(){
-    alert("Non lasciare il campo vuoto!")
-})
 
-// chiedo i km ed età + li valido + li salvo in una variabile
-// let userKm = parseInt(prompt("Quanti km devi fare?"));
+// nodi html km ed età
+const userKm = document.querySelector("[name='km']");
 
-while (isNaN(userKm)){
-    // userKm = parseInt(prompt("Inserisci un numero valido"));
-}
-
-// let userAge = parseInt(prompt("Quanti anni hai?"));
-
-while (isNaN(userAge)){
-    // userAge = parseInt(prompt("Inserisci un numero valido"));
-}
+const userAge = document.getElementById("age");
 
 // calolo il prezzo base del biglietto
-const priceFull = userKm * price;
+const priceFull = userKm.value * price;
 
-let priceFinal = priceFull;
-let disCount;
+// let priceFinal = priceFull;
+// let disCount;
 
 // calcolo lo sconto in base all'età
-if (userAge < 18){
-    disCount = .8;
-} else if (userAge >= 65) {
-    disCount = .6;
-}
+// if (userAge < 18){
+//     disCount = .8;
+// } else if (userAge >= 65) {
+//     disCount = .6;
+// }
 
-if (disCount){
-    priceFinal = parseInt((priceFull * disCount).toFixed(2));
-}
+// if (disCount){
+//     priceFinal = parseInt((priceFull * disCount).toFixed(2));
+// }
 
 console.log(priceFinal)
